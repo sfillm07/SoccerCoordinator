7da839de7dada7dec52e120667ea454f286c4df6
sfillm07 <sfillm07@yahoo.com>
2016-10-28 16:03:08 -0700

    Initial Commit

---
+++ SoccerCoordinator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoccerCoordinator
+//
+//  Created by Sean Fillmore on 10/28/16.
+//  Copyright © 2016 Sean Fillmore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
